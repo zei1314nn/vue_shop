349de03339e923a9dac177eaf62f76fdbca2cf6c
zei1314nn <zei1314nn@qq.com>
2020-10-18 20:51:34 +0800

    完成项目优化

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+// 这是项目发布阶段需要用到的 babel 插件
+const prodplugins = []
+if (process.env.MODE_ENV === 'production') {
+  prodplugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +15,9 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ]
+    ],
+    // 发布产品时候的插件数组
+    ...prodplugins,
+    "@babel/plugin-syntax-dynamic-import"
   ]
 }
---
+++ vue.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    // 选项...
+    chainWebpack: config => {
+        // 产品发布阶段默认打包入口  // 发布模式
+        config.when(process.env.NODE_ENV === 'production', config => {
+            config.entry('app').clear().add('./src/main-prod.js')
+
+            config.set('externals', {
+                vue: 'Vue',
+                'vue-router': 'VueRouter',
+                axios: 'axios',
+                lodash: '_',
+                echarts: 'echarts',
+                nprogress: 'NProgress',
+                'vue-quill-editor': 'VueQuillEditor'
+            })
+
+            config.plugin('html').tap(args => {
+                args[0].isProd = true
+                return args
+            })
+        })
+
+        // 产品开发阶段默认打包入口  // 开发模式
+        config.when(process.env.NODE_ENV === 'development', config => {
+            config.entry('app').clear().add('./src/main-dev.js')
+
+            config.plugin('html').tap(args => {
+                args[0].isProd = false
+                return args
+            })
+        })
+    }
+}
